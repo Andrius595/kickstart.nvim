6258a6990596e08e6f05a87a186ce10d0271db69
Andrius <andrius.cereska@ktu.edu>
2024-12-29 01:21:39 +0200

    Config from scratch

---
+++ after/queries/blade/highlights.scm
@@ -0,0 +1,8 @@
+(directive) @tag
+(directive_start) @tag
+(directive_end) @tag
+(comment) @comment
+((bracket_start) @function (#set! "priority" 120))
+((bracket_end) @function (#set! "priority" 120))
+(keyword) @function
+
---
+++ lua/plugins/colortheme.lua
@@ -0,0 +1,44 @@
+return {
+  'folke/tokyonight.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    require('tokyonight').setup {
+      style = 'night',
+      light_style = 'day',
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = 'transparent', -- style for sidebars, see below
+        floats = 'transparent', -- style for floating windows
+      },
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false,
+      on_colors = function(colors) end,
+      on_highlights = function(highlights, colors)
+        highlights.WinSeparator = { fg = '#555555', bg = 'NONE' }
+      end,
+      cache = true, -- When set to true, the theme will be cached for better performance
+      plugins = {
+        -- enable all plugins when not using lazy.nvim
+        -- set to false to manually enable/disable plugins
+        all = package.loaded.lazy == nil,
+        -- uses your plugin manager to automatically enable needed plugins
+        -- currently only lazy.nvim is supported
+        auto = true,
+        -- add any plugins here that you want to enable
+        -- for all possible plugins, see:
+        --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+        -- telescope = true,
+      },
+    }
+    vim.cmd [[colorscheme tokyonight]]
+  end,
+}
